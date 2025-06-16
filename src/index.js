aaada6fad65f2fd7de13f29a261af42a5b18cc7e
Sebastian Stockton <96149632+sebiprograms@users.noreply.github.com>
2025-06-16 15:08:29 -0500

    Update: display grid organizes div contents
    div items now are organized with grid the menu
    will have buttons allowing a change between
    pages using js, and below in display will be the pages
    contents.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import "./styles.css";
 
 const div = document.createElement('div')
+const menu = document.createElement('div')
+const display = document.createElement('div')
 const text = document.createElement('p')
-text.textContent = "testing webpack"
+text.textContent = "Example Webpage Using Webpack"
 div.classList.add("showcase")
-div.appendChild(text)
+menu.classList.add("menu")
+display.classList.add("display")
+
+
+div.appendChild(menu)
+menu.appendChild(text)
+div.appendChild(display)
 document.body.appendChild(div)
+
 
