1c817be7ea8ed2a1cdf940748d01e0e59629ab52
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-06-19 18:09:27 -0500

    Update: background image
    image changed to better fit theme also increased
    opacity of background for readability

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,12 @@
 import { div } from "./index.js"
 
+const contacts = document.createElement('div')
+const text = document.createElement('h2')
+text.textContent = "Contacts: "
+contacts.classList.add('contact')
+contacts.appendChild(text)
 
 export function contactpage() {
-  const contacts = document.createElement('div')
-  const text = document.createElement('h2')
-  text.textContent = "Contacts: "
-  contacts.classList.add('contact')
 
-  contacts.appendChild(text)
   div.appendChild(contacts)
 }
